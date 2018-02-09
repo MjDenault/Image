0e3fae88b5805ae5022d097f4da158a8fcb360ca
MjDenault <mj.denault@ocsbstudent.ca>
2018-02-09 13:14:21 -0500

    Initial commit

---
+++ main.lua
@@ -0,0 +1,12 @@
+-----------------------------------------------------------------------------------------
+--
+-- made: February 9 2018
+-- made by: Mj 
+-- the code displays a blue backround and a girl ninja gliding 
+-----------------------------------------------------------------------------------------
+
+ --Set default screen background color to blue 
+ display.setDefault ( "background",0, 0, 10 )
+ local image = display.newImageRect( "Glide_001.png", 1000, 1000 )
+ image.x = 1000
+ image.y = 1000
